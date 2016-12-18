2508d69680a6ba5cb9923d9a01e799f40768da5c
Ronan Gaillard <ronan.gaillard@live.fr>
2016-12-18 23:00:17 +0100

    Update script to generate learning base
    Now using the bitarray lib to store learning data as bits -> performance and less space on disk

--- generate-learning-base.py
+++ generate-learning-base.py
@@ -1,17 +1,18 @@
 import chess.pgn
+from bitarray import bitarray
 
-data_file = 'data/2016.pgn'
-
-
-i = 0
-
-def parse_game(game):
-    while not node.is_end():
-        next_node = node.variation(0)
-        node = next_node
-
+data_file = 'data/small_database.pgn'
+j = 0
 
 def game_list(filename):
+    """Generator that yields games in file indicated by filename
+
+    Args:
+        filename (str): the file path of the file to which games should be extracted
+
+    Returns:
+        single game extracted from file
+    """
     pgn = open(filename)
 
     while True:
@@ -24,10 +25,43 @@ def game_list(filename):
         yield game
 
 
-for game in game_list(data_file):
-    print i
-    i += 1
+if __name__ == '__main__':
+    with open("data.bin", 'wb') as binfile:
 
+        for game in game_list(data_file):
+            current_board = game.board()
 
-print i, "games read"
+            for move in game.main_line():
+                input_vector = bitarray(12*64) # 64 cases pour 12 pieces au total
+
+                # Ordre des pieces:
+                # 1 - tour blanche R
+                # 2 - cavalier blanc N
+                # 3 - fou blanc B
+                # 4 - dame blanche Q
+                # 5 - roi blanc K
+                # 6 - pion blanc P
+
+                piece_mapper = {'R': 0, 'N': 1, 'B': 2, 'Q': 3, 'K': 4, 'P': 5, \
+                                'r': 6, 'n': 7, 'b': 8, 'q': 9, 'k': 10, 'p': 11}
+
+                for i in range(0, 64):
+                    current_piece = str(current_board)[2*i]
+                    if current_piece == '.':
+                        continue
+                    input_vector[piece_mapper[current_piece]*64 + i] = True
+
+                # Save current position to file
+                input_vector.tofile(binfile)
+
+                # Go to next move in game
+                current_board.push(move)
+
+            # Used to indicate end of game
+            bitarray(12*64).tofile(binfile)
+
+            j += 1
+
+    print "Done ! Generated", j, " games !"
+
 
